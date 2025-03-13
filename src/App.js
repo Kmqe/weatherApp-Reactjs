eaaa8cc49d35e984c8f86e03358610f7743f88e9
kmqe <7moodi115b2@gmail.com>
2025-03-13 17:07:02 +0300

    upload the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,220 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Test from "./Test";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// REACT
+import { useEffect, useState } from "react";
+
+// MATERIAL UI COMPONENTS
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import CloudIcon from "@mui/icons-material/Cloud";
+import Button from "@mui/material/Button";
+
+// EXTERNAL LIBRARIES
+import axios from "axios";
+import moment from "moment";
+import "moment/min/locales";
+import { useTranslation } from "react-i18next";
+
+moment.locale("ar");
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["IBM"],
+  },
+});
+
+let cancelAxios = null;
 
 function App() {
+  const { t, i18n } = useTranslation();
+  // i18n.changeLanguage("ar");
+
+  // ============ STATES ============ //
+  const [dateAndTime, setDateAndTime] = useState();
+  const [temp, setTemp] = useState({
+    number: null,
+    description: "",
+    max: null,
+    min: null,
+    icon: null,
+  });
+  const [locale, setLocale] = useState("ar");
+
+  // ============ EVENT HANDLERS ============ //
+  function handleLanguageClick() {
+    console.log(locale);
+    if (locale === "en") {
+      setLocale("ar");
+      i18n.changeLanguage("ar");
+      moment.locale("ar");
+    } else {
+      setLocale("en");
+      i18n.changeLanguage("en");
+      moment.locale("en");
+    }
+
+    setDateAndTime(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  }
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+  }, []);
+
+  useEffect(() => {
+    setDateAndTime(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?lat=24.77&lon=46.73&appid=484a2d9e23dc059788f8b0752e275766";
+
+    axios
+      .get(apiUrl, {
+        cancelToken: new axios.CancelToken((c) => {
+          cancelAxios = c;
+        }),
+      })
+      .then(function (response) {
+        // handle success
+        const responseTemp = Math.round(response.data.main.temp - 272.15);
+        const min = Math.round(response.data.main.temp_min - 272.15);
+        const max = Math.round(response.data.main.temp_max - 272.15);
+        const description = response.data.weather[0].description;
+        let responseIcon = response.data.weather[0].icon;
+
+        setTemp({
+          number: responseTemp,
+          description: description,
+          max: max,
+          min: min,
+          icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+        });
+      })
+
+      .catch(function (error) {
+        // handle error
+        // console.log(error);
+      });
+
+    //
+    return () => {
+      cancelAxios();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          {/* CONTENT CONTAINER */}
+          <div className="content-container">
+            {/* CARD */}
+            <div className="card" dir={locale === "en" ? "lrt" : "rtl"}>
+              {/* CONTENT */}
+              <div>
+                {/* CITY & TIME */}
+                <div
+                  className="city-time"
+                  dir={locale === "en" ? "lrt" : "rtl"}
+                >
+                  <Typography
+                    variant="h2"
+                    style={{
+                      marginRight: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    {t("Riyadh")}
+                  </Typography>
+
+                  <Typography variant="h5" style={{ marginRight: "20px" }}>
+                    {dateAndTime}
+                  </Typography>
+                </div>
+                {/* == CITY & TIME == */}
+
+                <hr />
+
+                {/* CONTAINER OF DEGREE + CLOUD ICON */}
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  {/* DEGREE & DESCRIPTION */}
+                  <div>
+                    {/* TEMP */}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Typography variant="h1" style={{ textAlign: "right" }}>
+                        {temp.number}
+                      </Typography>
+
+                      {/* TODO: TEMP IMAGE */}
+                      <img src={temp.icon} alt="" />
+                    </div>
+                    {/*== TEMP ==*/}
+
+                    <Typography variant="h6">{t(temp.description)}</Typography>
+
+                    {/* MIN & MAX */}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <h5>
+                        {t("min")}: {temp.min}
+                      </h5>
+                      <h5 style={{ margin: "0px 5px" }}>|</h5>
+                      <h5>
+                        {t("max")}: {temp.max}
+                      </h5>
+                    </div>
+                  </div>
+                  {/*== DEGREE & DESCRIPTION ==*/}
+
+                  <CloudIcon
+                    style={{
+                      fontSize: "200px",
+                      color: "white",
+                    }}
+                  />
+                </div>
+                {/*= CONTAINER OF DEGREE + CLOUD ICON ==*/}
+              </div>
+              {/* == CONTENT == */}
+            </div>
+            {/*== CARD ==*/}
+
+            {/* TRANSLATION CONTAINER */}
+            <div
+              style={{
+                width: "100%",
+                textAlign: "left",
+                marginTop: "15px",
+              }}
+            >
+              <Button
+                style={{ color: "white" }}
+                variant="text"
+                onClick={handleLanguageClick}
+              >
+                {locale === "en" ? "Arabic" : "انجليزي"}
+              </Button>
+            </div>
+            {/*== TRANSLATION CONTAINER ==*/}
+          </div>
+          {/*== CONTENT CONTAINER ==*/}
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
